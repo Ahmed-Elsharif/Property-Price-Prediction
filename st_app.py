295f4c819e98b7df5747da39651b1e57956e8454
Ahmed Mostafa <87704226+Ahmed-Elsharif@users.noreply.github.com>
2024-08-28 20:54:36 +0300

    Add files via upload

--- st_app.py
+++ st_app.py
@@ -31,7 +31,7 @@ cities = {
     'Red Sea': ['Hurghada', 'El Gouna', 'Sahl Hasheesh'],
     'Suez': ['Suez', 'Ain Sokhna']
 }
-predicted_price = 0.00
+predicted_price = []
 def predict_price(area, bedroom_number, bathroom_number, property_type, governorate, city):
     # Create a DataFrame for the input data
     new_data = pd.DataFrame([[area, bedroom_number, bathroom_number, property_type, governorate, city]],
